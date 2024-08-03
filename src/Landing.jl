a2f0ab7ce0933bb00aa51a95889274754887682b
Marco Schwarz <marco.swz@hotmail.com>
2024-08-03 14:39:36 +0200

    Finish rocket landing

--- src/Landing.jl
+++ src/Landing.jl
@@ -1,3 +1,5 @@
+using JuliaSimControl.MPC: TerminalInput
+using Plots: debug!
 using JuliaSimControl
 using JuliaSimControl.MPC
 using JuliaSimControl.Symbolics
@@ -27,8 +29,8 @@ const thrust_max = 3*2300000/v_exhaust  # 701 kg/s. One Raptor thrust is 2.3 MN
 # Torque from engines thrust vestoring 
 const deflection_max = deg2rad(20)      # thrust vectoring +-20°
 const deflection_min = -deflection_max
-const torque_max = u_max * v_exhaust * length/2 * sin(deflection_max)
-const torque_min = -u_max *v_exhaust * length/2 * sin(deflection_max)
+const torque_max = thrust_max * v_exhaust * length/2 * sin(deflection_max)
+const torque_min = -thrust_max *v_exhaust * length/2 * sin(deflection_max)
 
 # Initial conditions
 const x_init = -600                  # [m] entry x coordinate
@@ -57,13 +59,13 @@ const torque_final = 0                # [N*m]
 
 # Number of mesh points (knots) to be used
 const n = 100
-const num_controls = 1
-const num_states = 3
-const num_steps = 200 # Optimization horizon (number of time steps)
+const num_controls = 2
+const num_states = 9
+const optimization_horizon = 10
 
 function rocket(state, control, _parameters, _=0)
     # Destructure state and control variables
-    y, x, angle, mass, d_y, d_x, d_angle, d_t = state
+    y, x, angle, mass, d_y, d_x, d_angle, d_t, fuel = state
     thrust, angle_thrust = control
 
     torque = -0.5 * length * v_exhaust * thrust * sin(angle_thrust)
@@ -83,11 +85,13 @@ function rocket(state, control, _parameters, _=0)
     d_y = d_y + dd_y * d_t
     d_angle = d_angle + dd_angle * d_t
 
-    return SA[y, x, mass, d_y, d_x, d_angle, d_t]
+    fuel = thrust
+
+    return SA[y, x, angle, mass, d_y, d_x, d_angle, d_t, fuel]
 end
 
-state_init = Float64[y_init, x_init, mass_init, d_y_init, d_x_init, d_angle_init, sample_time]
-state_final = Float64[y_final, x_final, mass_dry, d_y_final, d_x_final, d_angle_final, sample_time]
+state_init = Float64[y_init, x_init, angle_init, mass_init, d_y_init, d_x_init, d_angle_init, sample_time, 0]
+state_final = Float64[y_final, x_final, angle_final, mass_dry, d_y_final, d_x_final, d_angle_final, sample_time, 0]
 
 # The entire state is available for measurement
 measurement = (x, u, p, t) -> x 
@@ -95,21 +99,21 @@ measurement = (x, u, p, t) -> x
 dynamics = FunctionSystem(
     rocket, 
     measurement; 
-    x=[:y, :x, :mass, :d_y, :d_x, :d_angle, :d_t], 
+    x=[:y, :x, :angle, :mass, :d_y, :d_x, :d_angle, :d_t, :fuel], 
     u=[:thrust, :angle_thrust], y=:y^num_states
 )
 discrete_dynamics = MPC.rk4(dynamics, sample_time; supersample=3)
 
-lower_bounds = [0, -1500, -2*pi, mass_total, -80, -80, -deg2rad(45), sample_time, 0, deflection_min]
-upper_bounds = [y_init, 1500, 2*pi, mass_dry, 0, 80, deg2rad(45), sample_time, thrust_max, deflection_max]
+lower_bounds = [0, -1500, -2*pi, mass_total, -80, -80, -deg2rad(45), sample_time, 0, deflection_min, 0]
+upper_bounds = [y_init, 1500, 2*pi, mass_dry, 0, 80, deg2rad(45), sample_time, thrust_max, deflection_max, thrust_max]
 
 # Add lower and upper bounds
 stage_constraint = StageConstraint(lower_bounds, upper_bounds) do si, p, t
     # NOTE: The re-formating is required to align the structure of the state
     # with the format of the constraints.
-    y, x, angle, mass, d_y, d_x, d_angle, d_t = si.x
+    y, x, angle, mass, d_y, d_x, d_angle, d_t, fuel = si.x
     thrust, angle_thrust = si.u
-    return SA[y, x, angle, mass, d_y, d_x, d_angle, thrust, angle_thrust]
+    return SA[y, x, angle, mass, d_y, d_x, d_angle, d_t, thrust, angle_thrust, fuel]
 end
 
 # Add a terminal constraint for the final mass (it must be `mass_dry`)
@@ -118,49 +122,51 @@ terminal_constraint = TerminalStateConstraint([mass_dry], [mass_dry]) do ti, p, 
     return SA[mass]
 end
 
-interp_linear = Interpolations.LinearInterpolation([1, n], [state_init, state_final])
-initial_guess = Interpolations.mapreduce(transpose, vcat, interp_linear.(1:n))
-
-# Extract the initial guesses for state and control trajectories
-reference = zeros(num_states)
-objective_input = ObjectiveInput(state_trajectory, thrust_trajectory, reference)
-
 # Define an observer
-observer = StateFeedback(discrete_dynamics, state_initial)
+observer = StateFeedback(discrete_dynamics, state_init)
 
 # Define the loss and objective function
 loss = TerminalCost() do ti, p, t
     x = ti.x[2]
     d_x = ti.x[6]
-    thrust = ti.u[1]
+    thrust = ti.x[9]
     return sum(1*d_x.^2 + 1*thrust.^2 + 1*x.^2)
 end
 objective = Objective(loss)
 
+# Create objective input
+reference = zeros(num_states)
+thrust = zeros(num_controls, optimization_horizon)
+state_trajectory, thrust_trajectory = MPC.rollout(discrete_dynamics, state_init, thrust, 0, 0)
+objective_input = ObjectiveInput(state_trajectory, thrust_trajectory, reference)
+
 # Define the solver
 solver = IpoptSolver(;
     verbose=false,
-    tol=1e-8,
-    acceptable_tol=1e-5,
-    constr_viol_tol=1e-8,
-    acceptable_constr_viol_tol=1e-5,
-    acceptable_iter=10,
+    tol=1e-2,
+    acceptable_tol=1e-2,
+    constr_viol_tol=1e-2,
+    acceptable_constr_viol_tol=1e-2,
+    acceptable_iter=50,
+    exact_hessian=false,
 )
 
 # Define the nonlinear Model-Predictive Control problem
 problem = GenericMPCProblem(
     dynamics;
-    num_steps,
-    observer,
-    sample_time,
-    objective,
-    solver,
+    N=optimization_horizon,
+    observer=observer,
+    Ts=sample_time,
+    objective=objective,
+    solver=solver,
     constraints=[stage_constraint, terminal_constraint],
     objective_input=objective_input,
     xr=reference,
     presolve=true,
-    verbose=false,
-    jacobian_method=:symbolics, # generation of symbolic constraint Jacobians and Hessians are beneficial when using Trapezoidal as discretization.
+    verbose=true,
+    jacobian_method=:forwarddiff, # generation of symbolic constraint Jacobians and Hessians are beneficial when using Trapezoidal as discretization.
+    gradient_method=:reversediff,
+    hessian_method=:none,
     disc=Trapezoidal(; dyn=dynamics),
 )
 
@@ -170,8 +176,3 @@ plot(
     plot(u_sol', title="Control signal", lab=permutedims(input_names(dynamics))),
 )
 
-
-
-print(size(initial_guess))
-
-JuMP.set_start_value.(JuMP.all_variables(model), vec(initial_guess));    
